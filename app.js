ad647298a3c2c488e0a167507c84d19c9e1671bf
silviu4444 <73305252+silviu4444@users.noreply.github.com>
2021-01-12 22:05:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,92 +3,116 @@ const confirmPassword = document.querySelector('#confirmPassword');
 const button = document.querySelector('#button');
 const checkPw = document.querySelector('#checkPw');
 const checkConfPw = document.querySelector('#checkConfPw');
-const divPw = document.createElement('div');
-const divCheckPw = document.createElement('div');
-
-
+const errorMessagePw = document.createElement('div');
+const errorMessageCheckPw = document.createElement('div');
+const parentNode = password.parentNode;
 
 const checkPassword = () => {
-    let inputValue = password.value;
+    let inputPaswordValue = password.value;
     const passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/;
-    checkPw.appendChild(divPw);
-    const clearCheckPassword = () => {
-        if(password.value === "") {
-            checkPw.removeChild(divPw);
-        }
-    }
+    parentNode.insertBefore(errorMessagePw, confirmPassword);
 
-    if(inputValue === '') {
-        checkPw.removeChild(divPw);
-    }
-    else if(!inputValue.match(passw)) {
-        divPw.innerHTML = `<b>At least:</b>
+    const clearCheckPassword = () => {
+        if(inputPaswordValue === "" && confirmPassword.previousElementSibling === errorMessagePw) {
+            parentNode.removeChild(errorMessagePw);
+        }
+    };
+
+    if(inputPaswordValue === '' && confirmPassword.previousElementSibling === errorMessagePw) {
+        parentNode.removeChild(errorMessagePw);
+    } else if(!inputPaswordValue.match(passw)) {
+        errorMessagePw.innerHTML = `
+        <b>At least:</b>
         <li>1 uppercase letter</li>
         <li>1 lowercase letter</li>
         <li>1 number</li>`;
-        divPw.setAttribute('class', 'unvalid-password')
-        checkPw.appendChild(divPw);
-    }
-    else if(inputValue.match(passw)) {
-        divPw.textContent = "Valid Password";
-        divPw.setAttribute('class', 'valid-password')
-        checkPw.appendChild(divPw);
+        errorMessagePw.setAttribute('class', 'unvalid-password')
+        parentNode.insertBefore(errorMessagePw, confirmPassword);
+        checkConfPassword();
+    } else if(inputPaswordValue.match(passw)) {
+        errorMessagePw.textContent = "Valid Password";
+        errorMessagePw.setAttribute('class', 'valid-password');
+        parentNode.insertBefore(errorMessagePw, confirmPassword);
+        checkConfPassword();
     } 
-
     clearCheckPassword();
-}
+};
+
+const passwordBlurEvent = () => {
+    if(password.value === "" && confirmPassword.previousElementSibling === errorMessagePw) {
+        parentNode.removeChild(errorMessagePw);
+    } else if(password.value !== "" && confirmPassword.previousElementSibling === errorMessagePw) {
+        parentNode.removeChild(errorMessagePw);
+    }
+};
+
+const passwordFocusEvent = () => {
+    let inputPaswordValue = password.value;
+    const passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/;
+
+    if(!inputPaswordValue.match(passw)) {
+        errorMessagePw.innerHTML = `
+        <b>At least:</b>
+        <li>1 uppercase letter</li>
+        <li>1 lowercase letter</li>
+        <li>1 number</li>`;
+        errorMessagePw.setAttribute('class', 'unvalid-password')
+    }
+
+    parentNode.insertBefore(errorMessagePw, confirmPassword);
+};
 
 const checkConfPassword = () => {
     let inputPaswordValue = password.value;
     let inputConfrimPasswordValue = confirmPassword.value;
-    checkConfPw.appendChild(divCheckPw);
+    parentNode.insertBefore(errorMessageCheckPw, button);
+
     const clearCheckPassword = () => {
-        if(confirmPassword.value === "") {
-            checkConfPw.removeChild(divCheckPw);
+
+        if(inputConfrimPasswordValue === "") {
+            parentNode.removeChild(errorMessageCheckPw, button);
         }
-    }
+    };
 
     if(inputPaswordValue !== inputConfrimPasswordValue) {
-        divCheckPw.textContent = 'Passwords Do Not Matches!';
-        divCheckPw.setAttribute('class', 'unvalid-password');
-        checkConfPw.appendChild(divCheckPw);
+        errorMessageCheckPw.textContent = 'Passwords Do Not Matches!';
+        errorMessageCheckPw.setAttribute('class', 'unvalid-password');
+        parentNode.insertBefore(errorMessageCheckPw, button);
         button.disabled = true;
-    } 
-    else if(inputPaswordValue === inputConfrimPasswordValue) {
-        divCheckPw.textContent = 'Passwords Matches!';
-        divCheckPw.setAttribute('class', 'valid-password');
-        checkConfPw.appendChild(divCheckPw);
+    } else if(inputPaswordValue === inputConfrimPasswordValue) {
+        errorMessageCheckPw.textContent = 'Passwords Matches!';
+        errorMessageCheckPw.setAttribute('class', 'valid-password');
+        parentNode.insertBefore(errorMessageCheckPw, button);
         button.disabled = false;
     }
-    else if(inputValue === '') {
-        checkConfPw.removeChild(divCheckPw);
-    }
+
     clearCheckPassword();
-}
+};
 
-
-
-password.addEventListener('keyup', checkPassword)
-password.addEventListener('blur', () => {
-    if(password.value !== "") {
-        checkPw.removeChild(divPw);
+const confirmPasswordBlurEvent = () => {
+    if(confirmPassword === '' && button.previousElementSibling === errorMessageCheckPw) {
+        parentNode.removeChild(errorMessageCheckPw);
+    } else if(confirmPassword !== '' && button.previousElementSibling === errorMessageCheckPw) {
+        parentNode.removeChild(errorMessageCheckPw);
     }
-})
-password.addEventListener('focus', () => {
-    checkPw.appendChild(divPw);
-})
-confirmPassword.addEventListener('keyup', checkConfPassword)
-confirmPassword.addEventListener('blur', () => {
-    checkConfPw.removeChild(divCheckPw);
-})
-confirmPassword.addEventListener('focus', () => {
+};
+
+const confirmPasswordFocusEvent = () => {
     let inputPaswordValue = password.value;
     let inputConfrimPasswordValue = confirmPassword.value;
-    checkConfPw.appendChild(divCheckPw);
+    parentNode.insertBefore(errorMessageCheckPw, button);
 
     if(inputPaswordValue !== inputConfrimPasswordValue || inputPaswordValue === "") {
-        divCheckPw.textContent = 'Passwords Do Not Matches!';
-        divCheckPw.setAttribute('class', 'unvalid-password');
-        checkConfPw.appendChild(divCheckPw);
-    } 
-})
+        errorMessageCheckPw.textContent = 'Passwords Do Not Matches!';
+        errorMessageCheckPw.setAttribute('class', 'unvalid-password');
+        parentNode.insertBefore(errorMessageCheckPw, button);
+    }
+};
+
+password.addEventListener('keyup', checkPassword);
+password.addEventListener('blur', passwordBlurEvent);
+password.addEventListener('focus', passwordFocusEvent);
+
+confirmPassword.addEventListener('keyup', checkConfPassword);
+confirmPassword.addEventListener('blur', confirmPasswordBlurEvent);
+confirmPassword.addEventListener('focus', confirmPasswordFocusEvent);
